d8101657dd2dbae1062b5dc5293f029b3b1267ec
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2023-09-14 13:44:33 +0530

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -21,7 +21,7 @@ logger = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.INFO)
 logging.getLogger("telethon").setLevel(logging.INFO)
 
-ft = f"To use this bot you've to join @{fs}./n/Also join @mr_invisible_bots"
+ft = f"To use this bot you've to join @{fs}./n/Also join @official_satyam01"
 
 message = "Send me the message link you want to start saving from, as a reply to this message."
           
