63c7afdccd441c66144454c469d310289813cfd6
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2024-05-14 18:55:53 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -5,7 +5,7 @@ import json
 from main.plugins.helpers import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "█"
+FINISHED_PROGRESS_STR = "⚡"
 UN_FINISHED_PROGRESS_STR = ""
 DOWNLOAD_LOCATION = "/app"
 
@@ -47,7 +47,7 @@ async def progress_for_pyrogram(
             ),
         )  
 
-        tmp = progress + "GROSSS: {0} of {1}\n\nSpeed: {2}/s\n\nETA: {3}\n".format(
+        tmp = progress + "**GROSS⏰**: {0} of {1}\n\n**Speed🚀**: {2}/s\n\n**ETA🌐**: {3}\n\n**𝗥𝗔𝗗𝗛𝗘 𝗥𝗔𝗗𝗛𝗘 𝗕𝗥𝗢**".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
